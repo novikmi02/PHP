2720b2bffd7e47466bbb21b6aa013790d51af1de
novikmi02 <36410179+novikmi02@users.noreply.github.com>
2023-01-06 22:45:20 +0300

    Add files via upload

---
+++ TodoList/PHPWebPage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <?php
+    echo "Новый Год уже наступил?<BR><HR>";
+    ?>
+    <body>
+       <?php
+       $exp=true;
+       if($exp){?><strong>да</strong><?php}
+       else{?><strong>нет</strong><?php}
+       printf("<br>%s","Спасибо");
+       ?>
+    </body>
+</html>
